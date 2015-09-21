431a289382282091ec15ccefc4acd16b12d2ac12
Gladys Maniago <gfmaniago@gmail.com>
2015-09-21 16:44:57 -0500

    finished js and html

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,6 +1,48 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+var Router = Backbone.Router.extend({
+	routes: {
+		'loader': 'loader',
+		'menu': 'menu',
+		'play': 'play',
+		'leaders': 'leaders',
+		'settings': 'settings'
+
+	},
+	loader: function loader() {
+		$('section').hide();
+		$('#loader').show();
+	},
+	menu: function menu() {
+		$('section').hide();
+		$('#menu').show();
+	},
+	play: function play() {
+		$('section').hide();
+		$('#play').show();
+	},
+	leaders: function leaders() {
+		$('section').hide();
+		$('#leaders').show();
+	},
+	settings: function settings() {
+		$('section').hide();
+		$('#settings').show();
+	}
+
+});
+
+var page = new Router();
+Backbone.history.start();
+
+window.onload = function () {
+	page.navigate('loader', { trigger: true });
+	window.setTimeout(function () {
+		page.navigate('menu', { trigger: true });
+	}, 4000);
+};
+
 },{}]},{},[1])
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,46 @@
 'use strict';
+
+var Router = Backbone.Router.extend({
+	routes: {
+		'loader' : 'loader',
+		'menu' : 'menu',
+		'play': 'play',
+		'leaders': 'leaders',
+		'settings': 'settings'
+
+	},
+	loader: function(){
+		$('section').hide();
+		$('#loader').show();
+	},
+	menu: function(){
+		$('section').hide();
+		$('#menu').show();
+	},
+	play: function(){
+		$('section').hide();
+		$('#play').show();
+	},
+	leaders: function(){
+		$('section').hide();
+		$('#leaders').show();
+	},
+	settings: function(){
+		$('section').hide();
+		$('#settings').show();
+	}
+
+
+});
+
+var page = new Router();
+Backbone.history.start();
+
+
+window.onload =function(){
+		page.navigate('loader', {trigger: true});
+	window.setTimeout(function(){
+		page.navigate('menu', {trigger:true})
+	}, 4000);
+
+}
